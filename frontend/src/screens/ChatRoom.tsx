5acf63e6a0fe92d1541ff4d064f5554fad66dd55
ashutosh017 <wwwashutosh225@gmail.com>
2024-12-03 11:24:04 +0530

    making ui better

--- frontend/src/screens/ChatRoom.tsx
+++ frontend/src/screens/ChatRoom.tsx
@@ -1,13 +1,9 @@
-import { useEffect,  useRef, useState } from "react";
-import { useRecoilState } from "recoil";
-import {
-  iamgeAtom,
-  nameAtom,
-  roomIdAtom,
-  socketAtom,
-} from "../store/atom";
+import React, { useEffect, useRef, useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { iamgeAtom, nameAtom, roomIdAtom, socketAtom } from "../store/atom";
 import { message } from "../types";
 import { Button } from "../components/Button";
+import { IoMdSend } from "react-icons/io";
 import { CgAttachment } from "react-icons/cg";
 
 export const ChatRoom = () => {
@@ -19,18 +15,19 @@ export const ChatRoom = () => {
   const [selectedImage, setSelectedImage] = useRecoilState(iamgeAtom);
   const imageRef = useRef<HTMLInputElement>(null);
 
-  useEffect(()=>{
+  useEffect(() => {
     const roomId2 = localStorage.getItem("roomId") as string;
     const name2 = localStorage.getItem("name") as string;
     setRoomId(roomId2);
     setName(name2);
-    socket.send(JSON.stringify({
-      type:"new_joining",
-      roomId:roomId2,
-      name:name2
-    }))
-  },[])
-
+    socket.send(
+      JSON.stringify({
+        type: "new_joining",
+        roomId: roomId2,
+        name: name2,
+      })
+    );
+  }, []);
 
   const handleSendMessage = () => {
     const now = new Date();
@@ -82,43 +79,58 @@ export const ChatRoom = () => {
     }
   };
   return (
-    <div className="p-4 min-h-screen w-full bg-zinc-900 text-white ">
+    <div className="p-2 flex flex-col items-center h-screen w-full bg-zinc-900 text-white ">
       <div className="text-xl font-bold ">{roomId}</div>
-      <div>
-        <input
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              handleSendMessage();
-            }
-          }}
-          ref={messageRef}
-          placeholder="type your message"
-          className="p-2 rounded-md mr-2 text-black"
-          autoFocus
-        />
-        <Button
-          func={handleSendMessage}
-        >
-          Send message
+      <Messages />
+
+      <div className="flex  lg:bottom-8 lg:w-1/2 px-2 w-full space-x-2  ">
+        <div className="flex items-center border rounded-md bg-white text-black flex-grow">
+          <input
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleSendMessage();
+              }
+            }}
+            ref={messageRef}
+            placeholder="Type your message"
+            className="p-2 flex-grow rounded-l-md focus:outline-none"
+            autoFocus
+          />
+          <button
+            className="flex justify-center items-center px-3 hover:bg-gray-200 rounded-r-md"
+            onClick={() => {
+              messageRef.current?.focus();
+              imageRef.current?.click();
+            }}
+          >
+            <CgAttachment className="text-xl" />
+          </button>
+        </div>
+
+        <Button func={handleSendMessage}>
+          <IoMdSend className="size-6 mx-auto" />
         </Button>
       </div>
       <div className="">
-      <CgAttachment/>
-        <input type="file" ref={imageRef} onChange={handleFileChange} />
+        <input
+          type="file"
+          ref={imageRef}
+          onChange={handleFileChange}
+          className="hidden "
+        />
       </div>
-      <Messages />
     </div>
   );
 };
 
 const Messages = () => {
-  // const [socket, setSocket] = useRecoilState(socketAtom);
-  const socket = useRecoilState(socketAtom)[0];
-  const [messages, setMessages] = useState<message[]>([])
-  useEffect(() => {
-    localStorage.setItem("messages",JSON.stringify("[]"))
-    setMessages([])
-  }, []);
+  // const socket = useRecoilState(socketAtom)[0];
+  const socket = useRecoilValue(socketAtom);
+  const [messages, setMessages] = useState<message[]>([]);
+  // useEffect(() => {
+  //   localStorage.setItem("messages", JSON.stringify("[]"));
+  //   setMessages([]);
+  // }, []);
   useEffect(() => {
     localStorage.setItem("messages", JSON.stringify(messages));
   }, [messages]);
@@ -146,23 +158,41 @@ const Messages = () => {
       }
     };
   }, []);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  const prevMessagesRef = useRef(messages);
+
+  // Scroll to bottom only for new messages
+  useEffect(() => {
+    if (messages.length > prevMessagesRef.current.length) {
+      messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    }
+    prevMessagesRef.current = messages;
+  }, [messages]);
+  // useEffect(() => {
+  //   if (messagesEndRef.current) {
+  //     messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  //   }
+  // }, [messages]); // Trigger whenever `messages` change
 
   return (
-    <div>
+    <div className="border border-white w-full px-4 py-4 rounded-md flex-grow my-4 overflow-y-scroll">
       {messages.length > 0 &&
-        messages.map((msg, ind) => (
-          <div
-            key={ind}
-            className={`bg-zinc-800 w-3/4 rounded-md my-2 px-2 py-2 `}
-          >
-            <div className="text-md font-bold flex space-x-2">
-              <span className="">{msg.name}</span>
-              <span className="text-sm font-normal">{msg.date}</span>
-            </div>
-            <div className="text-wrap break-words">{msg.message}</div>
-            {msg.image && <img src={msg.image} alt="image" />}
-          </div>
-        ))}
+        messages.map((msg, ind) => <Message key={ind} msg={msg} />)}
+      <div ref={messagesEndRef} />
     </div>
   );
 };
+
+const Message = React.memo(({ msg }: { msg: message }) => {
+  return (
+    <div className={`bg-zinc-800 w-full rounded-md my-2 px-2 py-2 `}>
+      <div className="text-md font-bold flex space-x-2">
+        <span className="">{msg.name}</span>
+        <span className="text-sm font-normal">{msg.date}</span>
+      </div>
+      <div className="text-wrap break-words">{msg.message}</div>
+      {msg.image && <img src={msg.image} alt="image" />}
+    </div>
+  );
+})
